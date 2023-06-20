7552f1683070b27ce948744c7ae85ecef3cca1aa
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-06-20 10:01:11 +0530

    Added Changes In Leagues

--- Leagues.cpp
+++ Leagues.cpp
@@ -1,41 +1,45 @@
 # include <iostream>
 
 int main (){
+
+    // used variables
     int l ; 
     int r;
     int p;
     int s;
     
-    
+    // console print statements
     std::cout<<"----------------------------------------"<<'\n';
     std::cout<<"Press 1 to view la liga teams" <<'\n';
     std::cout<<"Press 2 to view Premier league teams : " <<'\n';
     std::cout<<"Press 3 to view Seria A teams : "  <<'\n';
     std::cout<<"----------------------------------------"<<'\n';
     
-    //entering the leauge 
+    //entering the leauge
     std::cout<<"Enter which league to view: " ;
     std::cin>>l;
      
-     // now the fucking hard part begins  
+     // name of the leagues and team playing in the league 
     std::string laliga[]={"Real Madrid","Barcelona","Atletico Madrid", "Villareal"};
-    if(l = 1){
-         for( r =0 ;r<3;r++){
+    std::string PremierLeague[] = {"Manchester United","Liverpool","Chelsea","Manchester City"};
+    std:: string SerieA [] = {"Juventus","AC Milan","Inter Milan","Napoli"};
+
+    // conditional checking
+    if(l==1){
+         for(r=0;r<4;r++) {
              std::cout<<laliga[r];
+             std::cout<<'\n';
          }
-   }else if (l=2){
-         std::string PremierLeague[] = {"Manchester United","Liverpool","Chelsea","Manchester City",};
-         for(p = 0;p<3;p++){
+   }else if (l==2){
+         for(p=0;p<4;p++) {
              std::cout<<PremierLeague[p];
+             std::cout<<'\n';
          }
    }else { 
-        std:: string SerieA [] = {"Juventus","AC milan","Inter Milan","Napoli"};
-       for(s = 0 ;s<3;s++){
+       for(s=0;s<4;s++) {
             std::cout<<SerieA[s];
+            std::cout<<'\n';
         }
    } 
     return 0;
 }
-     
-     
-
